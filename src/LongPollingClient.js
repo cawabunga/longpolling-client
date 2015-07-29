919aec10099a8d9f7aa8727748cd14041104e0fb
Emil Ibatullin <emil.ibatullin@gmail.com>
2015-07-29 17:32:06 +0300

    Transform response to JSObject on application/json

--- src/LongPollingClient.js
+++ src/LongPollingClient.js
@@ -65,7 +65,7 @@
                     if (this.readyState !== this.DONE) return;
 
                     if (200 <= this.status && this.status <= 308) {
-                        that.config.success(this.responseText);
+                        that.config.success(that.processResponse(this));
                         that.loop();
                     } else {
                         that.config.failure(this);
@@ -82,6 +82,21 @@
              * @private
              */
             afterRequest: function () {
+            },
+
+            /**
+             * @param  {XMLHttpRequest} xhr
+             * @returns {(Object|string)}
+             */
+            processResponse: function (xhr) {
+                var contentType = xhr.getResponseHeader('Content-type'),
+                    response = xhr.responseText;
+
+                if (contentType.indexOf('application/json') === 0) {
+                    response = JSON.parse(response);
+                }
+
+                return response;
             }
         };
 
